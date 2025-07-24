6207c83255794079c215baef6d5dc70bedae3f70
Adrián P. Blunier <adrian@numax.org>
2025-07-24 09:51:28 +0200

    Added Laravel Service Provider

---
+++ src/LaravelDinahostingServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NumaxLab\Laravel\Dinahosting;
+
+use Illuminate\Support\ServiceProvider;
+use NumaxLab\Laravel\Dinahosting\Console\Commands\DinahostingSymlink;
+
+class LaravelDinahostingServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                DinahostingSymlink::class,
+            ]);
+        }
+    }
+}
